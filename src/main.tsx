792051e15ad84d4cf11f8afe1d195ca53a9dccd4
KalimovR <kalimovr@gmail.com>
2026-01-06 05:58:22 +0500

    ДА

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,16 @@
+import 'crypto-browserify';
+import { randomUUID } from 'crypto';
+
+if (!globalThis.crypto) {
+  // @ts-ignore
+  globalThis.crypto = {};
+}
+
+if (!globalThis.crypto.randomUUID) {
+  // @ts-ignore
+  globalThis.crypto.randomUUID = randomUUID;
+}
+
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
